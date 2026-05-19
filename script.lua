4445f2f1c35c1bf6d3ee4a9b30b57cedd2287b15
ryanfelix8111-sketch <ryanfelix8111@gmail.com>
2026-05-19 16:29:11 -0300

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,9 @@
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+
+ScreenGui.Parent = game.CoreGui
+
+Frame.Parent = ScreenGui
+Frame.Size = UDim2.new(0,200,0,100)
+Frame.Position = UDim2.new(0.3,0,0.3,0)
+Frame.BackgroundColor3 = Color3.fromRGB(30,30,30)
